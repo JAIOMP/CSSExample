1267805f34799a266bbb5ca651848f71fe6c0569
Jai Om Pandey <jaiomp@thoughtworks.com>
2015-11-16 11:37:56 +0530

    now i am going to add a commit

--- localServer.js
+++ localServer.js
@@ -1,14 +1,28 @@
 var http = require("http");
 var fs = require("fs");
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+today = mm+'/'+dd+'/'+yyyy;
 var requestHandler = function(req,res){
-	if(req.url=='/'){
-	res.end(fs.readFileSync("./index.html"));
+	console.log(req.url);
+	if(req.method=="POST"){
+		req.on('data',function(data){
+			data = data.toString();
+			data = data.replace(/&/," ")
+			var fileData = fs.readFileSync("./guestBook.html","UTF-8").split("\n");
+			console.log(data,today,fileData);
+			var writtenComment = fileData.splice(32,0,data+today+"<br>");
+			console.log(writtenComment.join("\n"),fileData.splice(32,0,data+today+"<br>").join("\n"));
+			res.write(fi)
+
+		})
 	}
-	else{
-		console.log(+req.url)
-		var data = fs.readFileSync('.'+req.url)
-	}
-	res.end(data);
+	req.url=='/'?res.end(fs.readFileSync("./index.html"))
+	:res.end(fs.readFileSync('.'+req.url));
+
+
 };
 var server = http.createServer(requestHandler);
 server.listen(8000);
